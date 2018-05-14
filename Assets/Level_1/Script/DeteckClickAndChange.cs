4f3fccb5a86b05f8b34c16704b1adf764d7b40fd
Alexandre Levy <a.levy@equinoa.com>
2018-05-14 15:36:12 +0200

    First test change color of GO when click

---
+++ Assets/Level_1/Script/DeteckClickAndChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteckClickAndChange : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+            
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnMouseDown()
+    {
+        GameObject[] blocksToChange;
+        string tag;
+
+        //A déplacer dans le GameManager
+        var colorBlockArray = new Dictionary<string, Color>();
+        colorBlockArray.Add("red", Color.red);
+        colorBlockArray.Add("yellow", Color.yellow);
+        colorBlockArray.Add("black", Color.black);
+
+
+        tag = this.gameObject.tag;
+
+        //We search every GO with the tag of the color selected
+        blocksToChange = GameObject.FindGameObjectsWithTag(tag);
+
+        foreach (GameObject block in blocksToChange)
+        {
+            block.GetComponent<Renderer>().material.color = colorBlockArray[tag];
+        }
+
+    }
+}
